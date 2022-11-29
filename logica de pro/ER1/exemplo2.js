2880a5a62ae9fa39a170e98c3b7a0dbc54e91366
pedro hugo motta pifano <hugopedro331@gmail.com>
2022-11-29 01:17:14 -0300

    exemplo 1 e 2

---
+++ logica de pro/ER1/exemplo2.js
@@ -0,0 +1,16 @@
+//sistema para um radadar de velocidade 
+
+//variaveis
+let velocidade;
+
+//entrada
+velocidade = 100;
+
+//processamento/saida
+
+if (velocidade >100){
+    console.log("voce foi multado" )
+
+}
+
+//saida
