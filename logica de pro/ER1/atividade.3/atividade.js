81f7713cdb382e92f4a17f8edea0737b51fff898
pedro hugo motta pifano <hugopedro331@gmail.com>
2022-12-06 18:12:39 -0300

    resolução primeiro exercicio da primeira aula

---
+++ logica de pro/ER1/atividade.3/atividade.js
@@ -0,0 +1,90 @@
+let listadepecas = [];
+let peso = 150
+
+
+listadepecas.push("disco de freio", "FURADEIRA", "prego", "martelo", "parafuso", "chave philips", "chave de fenda", "torques", "chave torx", "roda", "pneu")
+
+if (listadepecas.length < 10){
+    console.log("espaço sobrando")
+}else{
+    console.log ("nao tem mais espaço na caixa")
+}
+
+if (peso<100){
+    console.log("a peça deve pesar mais que 100 gramas")
+}else{
+    console.log("essa peça possui o peso adequado")
+}
+
+let nomep0 = listadepecas[0]
+if (nomep0.length>3){
+    console.log("nome adequado para o cadastro")
+}else{
+    console.log("nao foi possivel cadastrar, favor inserir mais de 3 caracteres")
+}
+
+let nomep1 = listadepecas[1]
+if (nomep1.length>3){
+    console.log("nome adequado para o cadastro")
+}else{
+    console.log("nao foi possivel cadastrar, favor inserir mais de 3 caracteres")
+}
+
+let nomep2 = listadepecas[2]
+if (nomep2.length>3){
+    console.log("nome adequado para o cadastro")
+}else{
+    console.log("nao foi possivel cadastrar, favor inserir mais de 3 caracteres")
+}
+
+let nomep3 = listadepecas[3]
+if (nomep3.length>3){
+    console.log("nome adequado para o cadastro")
+}else{
+    console.log("nao foi possivel cadastrar, favor inserir mais de 3 caracteres")
+}
+
+let nomep4 = listadepecas[4]
+if (nomep4.length>3){
+    console.log("nome adequado para o cadastro")
+}else{
+    console.log("nao foi possivel cadastrar, favor inserir mais de 3 caracteres")
+}
+
+let nomep5 = listadepecas[5]
+if (nomep5.length>3){
+    console.log("nome adequado para o cadastro")
+}else{
+    console.log("nao foi possivel cadastrar, favor inserir mais de 3 caracteres")
+}
+
+let nomep6 = listadepecas[6]
+if (nomep6.length>3){
+    console.log("nome adequado para o cadastro")
+}else{
+    console.log("nao foi possivel cadastrar, favor inserir mais de 3 caracteres")
+}
+
+let nomep7 = listadepecas[7]
+if (nomep7.length>3){
+    console.log("nome adequado para o cadastro")
+}else{
+    console.log("nao foi possivel cadastrar, favor inserir mais de 3 caracteres")
+}
+
+let nomep8 = listadepecas[8]
+if (nomep8.length>3){
+    console.log("nome adequado para o cadastro")
+}else{
+    console.log("nao foi possivel cadastrar, favor inserir mais de 3 caracteres")
+}
+
+let nomep9 = listadepecas[9]
+if (nomep9.length>3){
+    console.log("nome adequado para o cadastro")
+}else{
+    console.log("nao foi possivel cadastrar, favor inserir mais de 3 caracteres")
+}
+
+
+console.log(listadepecas[1])
